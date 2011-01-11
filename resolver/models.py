403d350cf550806040e3481a4a5b86425d11fca3
Martin Malmsten <martin.malmsten@gmail.com>
2011-01-12 00:21:02 +0100

    changes to resolver/models.py

--- resolver/models.py
+++ resolver/models.py
@@ -4,6 +4,8 @@ class Prefix(models.Model):
     prefix = models.CharField(max_length=255, unique=True)
     redirect_url = models.CharField(max_length=255, blank=True)
     redirect_on_miss = models.BooleanField(default=False)
+    mint = models.BooleanField(default=False)
+    next_id = models.IntegerField(default=0)
 
     def __unicode__(self):
         return self.prefix
@@ -17,6 +19,7 @@ FEED_CHOICES = (
 class Feed(models.Model):
     prefix = models.ForeignKey(Prefix)
     type = models.CharField(max_length=32, choices=FEED_CHOICES)
+    refresh_rate = models.IntegerField(default=10*60)
     base_url = models.CharField(max_length=255)
     archive_objects = models.BooleanField(default=False)
     xslt_transform = models.TextField(blank=True)
@@ -26,10 +29,12 @@ class Feed(models.Model):
 
 
 class Identifier(models.Model):
-    prefix = models.ForeignKey(Prefix)
-    feed = models.ForeignKey(Feed)
+    prefix = models.ForeignKey(Prefix) #, blank=True, null=True, on_delete=models.SET_NULL)
+    feed = models.ForeignKey(Feed) #, blank=True, null=True, on_delete=models.SET_NULL)
     identifier = models.CharField(max_length=255, unique=True)
-    
+    reload_check = models.BooleanField(default=False)
+    deleted = models.BooleanField(default=False)
+
     def __unicode__(self):
         return self.identifier
 
@@ -39,6 +44,9 @@ class URL(models.Model):
     url = models.CharField(max_length=255)
     content_type = models.CharField(max_length=64, blank=True)
     default = models.BooleanField(default=True)
+    url_check = models.BooleanField(default=False)
+    last_ok = models.DateTimeField(blank=True)
+    n_retries = models.IntegerField(default=0)
 
     def __unicode__(self):
         return self.url + ", default=" + str(self.default) + ", content-type='" + self.content_type + "'"
@@ -46,5 +54,6 @@ class URL(models.Model):
 
 class Metadata(models.Model):
     identifier = models.ForeignKey(Identifier)
-    schema = models.CharField(max_length=32)
+    schema = models.CharField(max_length=128)
     data = models.TextField()
+
